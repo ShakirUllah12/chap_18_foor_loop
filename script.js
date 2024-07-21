21adf58ed857ed09c23eabb45f82050c5a26f4c2
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-22 01:24:15 +0500

    chap 18  done

---
+++ script.js
@@ -0,0 +1,41 @@
+// // chapter nomber 18  foor loop 
+
+// const clenesCity=["peshawar","islamabad","mardan","kohat"];
+// const cityToCheck=prompt("please enter your city name");
+
+// if (cityToCheck===clenesCity[0]);{
+//     alert("its one of the clenescit");
+// }
+// else if (cityToCheck===clenesCity[1]){
+//         alert("its one of the clenescit");
+//     }
+
+//     else if (cityToCheck===clenesCity[2]){
+//         alert("its one of the clenescit");
+//     }
+
+//          else if (cityToCheck===clenesCity[3]);{
+//         alert("its one of the clenescit");
+//     }   
+//         else if (cityToCheck===clenesCity[4]){
+//                 alert("its one of the clenescit");
+//             }
+
+// else{
+//     alert("it is not on the list");
+// }
+
+// for ( let number=1;number <10;number++){
+//     console.log(number);
+// }
+
+// for ( let number=0;number <10;number++){
+//     console.log(number);
+// }
+
+// const clenesCity=["peshawar","islamabad","mardan","kohat"];
+// const cityToCheck=prompt("please enter your city name");
+
+// for (let cityIndex=0;cityIndex<clenesCity.length;cityIndex++){
+//     alert("it one of the clenes city");
+// }
